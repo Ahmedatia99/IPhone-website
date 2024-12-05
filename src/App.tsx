9f7be0dca908c848886d77a9836d06d4ea511ce3
Ahmedatia99 <atyaa629@gmail.com>
2024-12-05 07:35:20 +0200

    add sentry

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Navbar from "./component/Navbar.jsx";
 import Hero from "./component/Hero.jsx";
 import Highlight from "./component/Highlight.jsx";
 import Model from "./component/Model.jsx";
+import * as Sentry from "@sentry/react";
+
 const App = () => {
   return (
     <div className="bg-black ">
@@ -13,4 +15,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);

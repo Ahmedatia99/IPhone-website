fffdfccaed406354a22d84d870f7f37f25b294aa
Ahmedatia99 <atyaa629@gmail.com>
2024-12-05 06:37:18 +0200

    orbit iphone

--- src/component/Model.jsx
+++ src/component/Model.jsx
@@ -1,19 +1,16 @@
-import React from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-
 import ModelView from "./ModelView";
+import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
-import { models, sizes } from "../constants";
-import * as Three from "three";
+
+import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
-
-import { useState, useRef, useEffect } from "react";
-import { animateWithGsapTimeline } from "../utils/animation";
+import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animation.js";
 
 const Model = () => {
-  // initial state to model view = small
   const [size, setSize] = useState("small");
   const [model, setModel] = useState({
     title: "iPhone 15 Pro in Natural Titanium",
@@ -22,15 +19,16 @@ const Model = () => {
   });
 
   //camera control => model view
-  const cameraControlLarge = useRef();
   const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
 
-  const large = useRef(new Three.Group());
-  const small = useRef(new Three.Group());
+  // model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
 
-  //   Rotation control => model view
-  const [smallRotation, setSmallRotation] = useState();
-  const [largeRotation, setLargeRotation] = useState();
+  // rotation
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
 
   const tl = gsap.timeline();
 
@@ -51,43 +49,46 @@ const Model = () => {
   }, [size]);
 
   useGSAP(() => {
-    gsap.to("#heading", { opacity: 1, y: 0 });
+    gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
 
   return (
     <section className="common-padding">
       <div className="screen-max-width">
         <h1 id="heading" className="section-heading">
-          Take a closer look{" "}
+          Take a closer look.
         </h1>
-        <div className="flex flex-col -items-center mt-5">
+
+        <div className="flex flex-col items-center mt-5">
           <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
             <ModelView
               index={1}
               groupRef={small}
+              gsapType="view1"
               controlRef={cameraControlSmall}
               setRotationState={setSmallRotation}
-              gsapType="view1"
               item={model}
               size={size}
             />
+
             <ModelView
               index={2}
               groupRef={large}
+              gsapType="view2"
               controlRef={cameraControlLarge}
               setRotationState={setLargeRotation}
-              gsapType="view2"
               item={model}
               size={size}
             />
+
             <Canvas
               className="w-full h-full"
               style={{
                 position: "fixed",
                 top: 0,
-                right: 0,
-                left: 0,
                 bottom: 0,
+                left: 0,
+                right: 0,
                 overflow: "hidden",
               }}
               eventSource={document.getElementById("root")}
@@ -95,8 +96,10 @@ const Model = () => {
               <View.Port />
             </Canvas>
           </div>
-          <div className="w-full mx-auto">
+
+          <div className="mx-auto w-full">
             <p className="text-sm font-light text-center mb-5">{model.title}</p>
+
             <div className="flex-center">
               <ul className="color-container">
                 {models.map((item, i) => (
@@ -108,6 +111,7 @@ const Model = () => {
                   />
                 ))}
               </ul>
+
               <button className="size-btn-container">
                 {sizes.map(({ label, value }) => (
                   <span
